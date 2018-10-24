e1d2c180feca4274a8cd14023ecbf0b335672220
D O <daria.osipova@nure.ua>
2018-10-24 11:11:17 +0300

    changed task2

--- SPZ_l1_v1/task2/Task2.cs
+++ SPZ_l1_v1/task2/Task2.cs
@@ -12,44 +12,49 @@ namespace SPZ_l1_v1.task2
         public static void Test()
         {
             Console.WriteLine(NeedTo);
-            Console.WriteLine("Enter day");
-            bool t = Int32.TryParse(Console.ReadLine(), out int d);
-            Console.WriteLine("Enter mounth");
-            bool t2 = Int32.TryParse(Console.ReadLine(), out int m);
-            if(!t || !t2 || m > 12 || m < 1 || d < 1 || d > 31)
-                    Console.WriteLine("Mistake");
-            else
+            while (true)
             {
-                switch (m)
+                Console.WriteLine("Enter day");
+                bool t = Int32.TryParse(Console.ReadLine(), out int d);
+                if (!t)
+                    break;
+                Console.WriteLine("Enter mounth");
+                bool t2 = Int32.TryParse(Console.ReadLine(), out int m);
+                if (!t2)
+                    break;
+                if (!t || !t2 || m > 12 || m < 1 || d < 1 || d > 31)
+                    Console.WriteLine("Mistake");
+                else
                 {
-                    case 1:
-                    case 3:
-                    case 5:
-                    case 7:
-                    case 8:
-                    case 10:
-                    case 12:
+                    switch (m)
+                    {
+                        case 1:
+                        case 3:
+                        case 5:
+                        case 7:
+                        case 8:
+                        case 10:
+                        case 12:
                             if (d == 31)
                                 Console.WriteLine("Correct");
-                        else Console.WriteLine("Uncorrect");
-                        break;
-                    case 2:
-                        if(d == 29 || d == 28)
-                            Console.WriteLine("Correct");
-                        else Console.WriteLine("Uncorrect");
-                        break;
-                    case 4:
-                    case 6:
-                    case 9:
-                    case 11:
-                        if (d == 30)
-                            Console.WriteLine("Correct");
-                        else Console.WriteLine("Uncorrect");
-                        break;
+                            else Console.WriteLine("Uncorrect");
+                            break;
+                        case 2:
+                            if (d == 29 || d == 28)
+                                Console.WriteLine("Correct");
+                            else Console.WriteLine("Uncorrect");
+                            break;
+                        case 4:
+                        case 6:
+                        case 9:
+                        case 11:
+                            if (d == 30)
+                                Console.WriteLine("Correct");
+                            else Console.WriteLine("Uncorrect");
+                            break;
+                    }
                 }
-                
             }
         }
-
     }
 }
